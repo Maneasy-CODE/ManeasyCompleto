8572b9797c89f440498dd14c430abf7ce70c86fb
Gustavo Luiz <98906854+guusluiz@users.noreply.github.com>
2024-02-19 19:58:39 -0300

    Delete ManeasyHtmlCss directory

--- ManeasyHtmlCss/consulta/consulta.js
+++
@@ -1,8 +0,0 @@
-let input_consulta = document.getElementById("input_consulta");
-
-// function fundo_amarelo(){
-if (window.getComputedStyle(input_consulta).backgroundColor == "rgb(255, 255, 255)") {
-    // input_projeto.setAttribute( "style", "background-color", "rgb(255 209 0)")
-    input_consulta.style.backgroundColor = "var(--amarelo)"
-}
-// }   
--- ManeasyHtmlCss/projeto_cadastro/projeto_cadastro.js
+++
@@ -1,8 +0,0 @@
-let input_projeto = document.getElementById("input_projeto");
-
-// function fundo_amarelo(){
-if (window.getComputedStyle(input_projeto).backgroundColor == "rgb(255, 255, 255)") {
-    // input_projeto.setAttribute( "style", "background-color", "rgb(255 209 0)")
-    input_projeto.style.backgroundColor = "var(--amarelo)"
-}
-// }   
